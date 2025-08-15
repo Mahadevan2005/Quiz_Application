50b4e7d143d00e31634d84f0f2ee363e6735bb98
M.Mahadevan <mahadevan122005@gmail.com>
2025-08-15 18:20:49 +0530

    Added instructions for the test

--- src/components/InstructionsDialog.jsx
+++ src/components/InstructionsDialog.jsx
@@ -12,12 +12,14 @@ import {
 
 export default function InstructionsDialog({ open, onStart, onCancel }) {
   const rules = [
-    "You have 30 minutes to complete the quiz.",
-    "Once started, the timer will not pause.",
-    "Do not refresh or close the browser during the quiz.",
-    "Questions can be navigated in any order.",
-    "Auto-submit will happen when time runs out.",
-    "Once submitted, answers cannot be changed.",
+    "1. You have 30 minutes to complete the quiz.",
+    "2. Once started, the timer will not pause.",
+    "3. Do not refresh or close the browser during the quiz.",
+    "4. Questions can be navigated in any order.",
+    "5. Auto-submit will happen when time runs out.",
+    "6. Once submitted, answers cannot be changed.",
+    "7. Review your answer before submitting it.",
+    "8. All the best!!!"
   ];
 
   return (
